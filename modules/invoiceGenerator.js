3588a520954cb45b62534e10fac60e88ec093ccf
Ernesto Acosta <ernesto.acosta1086@gmail.com>
2023-10-31 22:21:01 -0400

    Added status style depends on value

--- modules/invoiceGenerator.js
+++ modules/invoiceGenerator.js
@@ -13,14 +13,14 @@ class InvoiceGenerator {
 
   static generateInvoiceItem(invoiceData, invoiceDiv) {
     const totalFixed = invoiceData.total.toFixed(2)
+    const status = invoiceData.status.charAt(0).toUpperCase() + invoiceData.status.slice(1)
     // Llenar el div con los datos de la factura
     invoiceDiv.innerHTML = `
-        <spam id="id"><spam>#</spam>${invoiceData.id}</spam>
-        <spam id="date">Due ${invoiceData.createdAt}</spam>
-        <spam id="clientName">${invoiceData.clientName}</spam>
-        
-        <spam id="total">£ ${totalFixed}</spam>
-        <spam id="status">${invoiceData.status}</spam>
+        <span id="id"><span>#</span>${invoiceData.id}</span>
+        <span id="date">Due ${invoiceData.createdAt}</span>
+        <span id="clientName">${invoiceData.clientName}</span>
+        <span id="total">£ ${totalFixed}</span>
+        <span class="status ${invoiceData.status}">&#x25CF; ${status}</span>
         <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l4 4-4 4" stroke="#7C5DFA" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
     `
   }
