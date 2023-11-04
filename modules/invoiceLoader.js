4bff390cc03a73f76107cf0b78bbf7e9e6921d6a
Ernesto Acosta <ernesto.acosta1086@gmail.com>
2023-11-03 21:05:36 -0400

    Filter invoice list depends on statuses

--- modules/invoiceLoader.js
+++ modules/invoiceLoader.js
@@ -1,5 +1,5 @@
 class InvoiceLoader {
-  static async invoiceLoad() {
+  static async genLoadInvoices() {
     try {
       const response = await fetch('./../data.json')
       if (response.ok) {
@@ -15,17 +15,38 @@ class InvoiceLoader {
     }
   }
 
-  static async invoiceFilteredLoad(statusSelected) {
+  // static async genInvoiceStatuses(statusSelected) {
+  //   try {
+  //     const response = await fetch('./../data.json')
+  //     if (response.ok) {
+  //       const invoiceData = await response.json()
+
+  //       statusSelected.forEach((status) => {
+  //         const filteredStatus = invoiceData.filter((invoice) => status.includes(invoice.status))
+  //         console.log(filteredStatus)
+  //       })
+  //       return filteredStatus
+  //     } else {
+  //       console.error('Error al cargar datos de factura')
+  //       return null
+  //     }
+  //   } catch (error) {
+  //     console.error('Error en la solicitud de factura:', error)
+  //     return null
+  //   }
+  // }
+
+  static async genInvoiceFilteredFromAPI(statusSelected) {
     try {
       const response = await fetch('./../data.json')
       if (response.ok) {
         const invoiceData = await response.json()
 
-        statusSelected.forEach((status) => {
-          const filteredStatus = invoiceData.filter((invoice) => status.includes(invoice.status))
-          console.log(filteredStatus)
-        })
-        return filteredStatus
+        // Filtrar facturas según los estados seleccionados
+        const filteredInvoices = invoiceData.filter((invoice) =>
+          statusSelected.includes(invoice.status)
+        )
+        return filteredInvoices
       } else {
         console.error('Error al cargar datos de factura')
         return null
--- script.js
+++ script.js
@@ -3,48 +3,75 @@ import InvoiceGenerator from './modules/invoiceGenerator.js'
 
 let filterMenu = document.querySelector('.action_menu-checkbox_menu')
 let filterMenuContainer = document.querySelector('.action_menu-checkbox_options')
+const invoiceContainer = document.querySelector('.invoice-list')
 
 //Variable to save all possible status
 let statusData = []
 
 async function startApp() {
-  const invoiceData = await InvoiceLoader.invoiceLoad()
+  const invoiceData = await InvoiceLoader.genLoadInvoices()
   if (invoiceData) {
-    console.log(invoiceData)
-    const invoiceContainer = document.querySelector('.invoice-list')
+    //Generate the list with all invoices
     InvoiceGenerator.generateInvoiceDom(invoiceData, invoiceContainer)
+    //Populate all posible statuses
     invoiceData.forEach((invoice) => {
       if (!statusData.includes(invoice.status)) {
         statusData.push(invoice.status)
       }
     })
   }
-  //Generate menu with all status
+  //Generate menu with all statuses
   InvoiceGenerator.generateFilterMenuDom(statusData, filterMenuContainer)
   let statusCheckBoxesItem = document.querySelectorAll('.action_menu-filter_item')
   let checkboxesStatus = document.querySelectorAll('.action_menu-checkbox')
 
-  //Array to populate filtered status
+  //Array to populate filtered statuses
   let statusFiltered = []
 
   statusCheckBoxesItem.forEach((checkboxItem, index) => {
-    checkboxItem.addEventListener('click', () => {
+    checkboxItem.addEventListener('click', async () => {
+      //Clean all elements from the invoice list before generate filtered invoices
+      while (invoiceContainer.firstChild) {
+        invoiceContainer.removeChild(invoiceContainer.firstChild)
+      }
+
       //On click if the ckeckbox is checked and the array doesnt have the value add this value to the array
       if (checkboxesStatus[index].checked && !statusFiltered.includes(statusData[index])) {
-        //Add the value to the array
+        //Add the current invoice to the array
         statusFiltered.push(statusData[index])
-        console.log(statusFiltered)
+        //Create an array with all filtered invoices await
+        const filteredInvoices = await genFilteredInvoices(statusFiltered)
+        //Generate the DOM elements to populate the invoice list
+        InvoiceGenerator.generateInvoiceDom(filteredInvoices, invoiceContainer)
       }
       //On click if the checkbox is not checked delete this value from the array
       else if (!checkboxesStatus[index].checked && statusFiltered.includes(statusData[index])) {
         statusFiltered = statusFiltered.filter((value) => value !== statusData[index])
-        console.log(statusFiltered)
+
+        //Create an array with all filtered invoices await
+        const filteredInvoices = await genFilteredInvoices(statusFiltered)
+        //Generate the DOM elements to populate the invoice list
+        InvoiceGenerator.generateInvoiceDom(filteredInvoices, invoiceContainer)
       }
     })
   })
 }
 
 startApp()
+
+//Function to await the filtered Invoices by statuses
+async function genFilteredInvoices(status) {
+  try {
+    const invoiceFilteredData = await InvoiceLoader.genInvoiceFilteredFromAPI(status)
+    if (invoiceFilteredData) {
+      return invoiceFilteredData
+    } else {
+      throw new Error('Error loadind the data')
+    }
+  } catch (error) {
+    throw error
+  }
+}
 
 //On click event to show/hide the filter menu
 filterMenu.addEventListener('click', () => {
